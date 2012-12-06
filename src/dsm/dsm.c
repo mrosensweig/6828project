b46d15b84707e0d884096898e1691ba43200cf44
Mike Rosensweig <mjsweig@mit.edu>
2012-12-05 23:36:30 -0500

    more stuff

--- src/dsm/dsm.c
+++ src/dsm/dsm.c
@@ -7,6 +7,20 @@ dsm_init(void)
 {
   sigsegv_init (&dispatcher);
   sigsegv_install_handler (&handler);
+
+  void *p;
+  unsigned long dsm_area;
+  
+  if ( (p = mmap( (void *) DSM_AREA_START, PGSIZE, PROT_READ_WRITE, 
+                      MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) < 0)
+  {
+    fprintf(stderr, "mmap failed\n");
+    exit (2);
+  }
+  dsm_area = (unsigned long) p;
+
+  sigsegv_register(&dispatcher,(void *)dsm_area,PGSIZE,&dsm_area_handler,&dsm_area);
+  
 }
 
 static int
