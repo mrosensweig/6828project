50ba2275767f3340b68be471c6608b00681edd0c
Mike Rosensweig <mjsweig@mit.edu>
2012-12-09 11:28:23 -0500

    fixed double faulting

--- src/dsm/dsm.c
+++ src/dsm/dsm.c
@@ -11,13 +11,18 @@ dsm_init(void)
   void *p;
   unsigned long dsm_area;
   
-  if ( (p = mmap( (void *) DSM_AREA_START, PGSIZE, PROT_READ_WRITE, 
+  if ( (p = mmap( (void *) DSM_AREA_START, PGSIZE * NPAGES, PROT_READ_WRITE, 
                       MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) < 0)
   {
     fprintf(stderr, "mmap failed\n");
     exit (2);
   }
   dsm_area = (unsigned long) p;
+
+  if (set_permissions((void *) dsm_area, PGSIZE * NPAGES, PROT_READ) < 0) {
+    fprintf(stderr, "mrpotect failed at %p\n", (void *)dsm_area);
+    exit(2);
+  }
 
   sigsegv_register(&dispatcher, 
                    (void *)dsm_area,
@@ -39,6 +44,7 @@ dsm_area_handler (void *fault_address, void *user_arg)
         fprintf(stderr, "Failure setting permissions at %p\n", fault_address);
         exit (2);
       }
+      return 1;
     } else if ((perms | PROT_READ) == PROT_READ) {
       // attempted to write, so we need to make writeable and then invalidate
       printf("found PROT_READ, changing to PROT_READ_WRITE\n");
@@ -47,14 +53,17 @@ dsm_area_handler (void *fault_address, void *user_arg)
         fprintf(stderr, "Failure setting write permissions at %p\n", fault_address);
         exit(2);
       }
+      return 2;
     } else {
       /*
       not supporting executes yet, so if we fault here there is a problem
       */
       fprintf(stderr, "Attempted Execute at %p\n", fault_address);
       exit(2);
+      return 3;
   }
-  printf("end of handler\n");
+  //printf("end of handler\n");
+  return 4;
 }
 
 static int
--- src/tests/dsm_tests.c
+++ src/tests/dsm_tests.c
@@ -6,12 +6,10 @@ main(int argc, char **argv)
   dsm_init();
   int *a = (int *) DSM_AREA_START;
 
-  set_permissions((void *)DSM_AREA_START, PGSIZE, PROT_READ);
-  
 printf("before\n");
   int ab = *a;
 printf("after: %p\n", &ab);
   *a = 17;
-
+*a = 32;
   printf("a = %d\n", *a);
 }
